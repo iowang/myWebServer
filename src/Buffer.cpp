6fb29e00ea9c0128e56b67b4bccb884e4f5fce19
iowang <wzh_9812@163.com>
2023-03-26 15:22:18 +0800

    complete function

---
+++ src/Buffer.cpp
@@ -0,0 +1,39 @@
+#include "Buffer.h"
+
+size_t Buffer::Size() const
+{
+    return buf_.size();
+}
+
+const std::string &Buffer::buf() const
+{
+    return buf_;
+}
+
+const char *c_str() const
+{
+    return buf_.c_str();
+}
+
+void Buffer::set_buf(const char *buf)
+{
+    std::string new_buf(buf);
+    buf_.swap(new_buf);
+}
+
+void Buffer::Append(const char *_str, int _size)
+{
+    for (int i = 0; i < _size; ++i)
+    {
+        if (str[i] == '\0')
+        {
+            break;
+        }
+        buf_.push_back(str[i]);
+    }
+}
+
+void Buffer::Clear()
+{
+    buf_.clear();
+}
--- src/Socket.cpp
+++ src/Socket.cpp
@@ -44,5 +44,91 @@ RC Socket::Listen() const
 
 RC Socket::Connect(const char *ip, uint16_t port) const
 {
+    assert(fd_ != -1);
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    socklen_t len = sizeof(addr);
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr(ip);
+    addr.sin_port = htons(port);
+    if (connect(fd_, (struct sockaddr *)&addr, len) == -1)
+    {
+        perror("Failed to connect socket");
+        return RC_SOCKET_ERROR;
+    }
+    return RC_SUCCESS;
+}
 
+RC Socket::Accept(int &clnt_fd) const
+{
+    assert(clnt_fd != -1);
+    clnt_fd = accept(fd_, NULL, NULL);
+    if (clnt_fd == -1)
+    {
+        perror("Failed to accept socket");
+        return RC_SOCKET_ERROR;
+    }
+    return RC_SUCCESS;
+}
+
+RC Socket::Create()
+{
+    assert(fd == -1);
+    fd_ = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd_ == -1)
+    {
+        perror("Failed to create socket");
+        return RC_SOCKET_ERROR;
+    }
+    return RC_SUCCESS;
+}
+
+void Socket::set_fd(int fd)
+{
+    fd_ = fd;
+}
+
+int Socket::fd()
+{
+    return fd_;
+}
+
+std::string Socket::get_addr() const
+{
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    socklen_t len = sizeof(addr);
+    if (getpeername(fd_, (struct sockaddr *)&addr, &len) = -1)
+    {
+        return "";
+    }
+    std::string ret(inet_ntoa(addr.sin_addr));
+    ret += ":";
+    ret += std::to_string(htons(addr.sin_port));
+    return ret;
+}
+
+RC SetNonBlocking() const
+{
+    if (fcntl(fd_, F_SETFL, fcntl(fd_, F_GETFL) | O_NONBLOCK) == -1)
+    {
+        perror("Socket set non-blocking failed");
+        return RC_SOCKET_ERROR;
+    }
+    return RC_SUCCESS;
+}
+
+bool IsNonBlocking() const
+{
+    return (fcntl(fd_, F_GETFL) & O_NONBLOCK) != 0;
+}
+
+size_t Socket::RecvBufSize() const
+{
+    size_t size = -1;
+    if (ioctl(fd_, FIONREAD, &size) == -1)
+    {
+        perror("Socket get recv buf size failed");
+    }
+    return size;
 }
